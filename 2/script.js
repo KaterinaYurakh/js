a8ae8301f5f57f5b23cfbce7e76315879d54fa0b
KaterinaYurakh <katyasalomatova1994@gmail.com>
2022-12-04 13:37:37 +0200

    2

---
+++ 2/script.js
@@ -0,0 +1,17 @@
+// №1
+
+const name = 'John';
+const admin = name;
+alert(admin);
+
+// №2
+
+const namePlanet = 'Earth';
+const currentUser = 'Pablo';
+
+
+// №3
+
+const sum = '10' + 10;
+console.log(sum);
+console.log(typeof sum);
